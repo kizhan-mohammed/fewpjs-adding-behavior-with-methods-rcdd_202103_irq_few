e7914a568858d0c4662e5d1afde076ba78b7b481
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-14 07:56:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,10 +26,17 @@ class Bird {
       this.sex = sex;
   }
     speak(){
+<<<<<<< HEAD
    if(this.sex === "male"){
      return`It's me! ${this.name}, the parrot!`
    }else{
      return`${this.name} says squawk!`
+=======
+   if(this.sex === male){
+     return`It's me! ${this.name}, the parrot!`
+   }else{
+     return`${this.name} squawk!`
+>>>>>>> 5c4e42616bae5491efa0a021a36774301c4ef92e
    }
   }
 }
